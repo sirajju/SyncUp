8ef5b5c452d1e898e08bfb6c692ab737286f2a56
sirajju <munnas2aa@gmail.com>
2024-01-31 19:34:46 +0530

    videocall ui improved

--- client/src/Components/VideoCall/VideoCall.jsx
+++ client/src/Components/VideoCall/VideoCall.jsx
@@ -9,15 +9,13 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import { setChat } from '../../Context/userContext';
 
-function VideoCall({ hangUpCall, declineCall }) {
+function VideoCall(props) {
   const socket = useSocket()
-  const userData = useSelector(state => state.user)
-  const chat = useSelector(state => state.chat)
+  const [userData, setUserData] = useState({})
   const conversationRef = useRef(null);
-  const dispatch = useDispatch()
-  const [conversationName, setConversationName] = useState("")
-  const [isConnected, setConnected] = useState(false)
+  const [chat, setChat] = useState(props.chat)
   const [isLoaded, setLoaded] = useState(false)
+  const [localStream, setLocalStream] = useState(null)
   const callState = useSelector(state => state.call)
   const onStreamListChangedHandler = function (streamInfo) {
     if (streamInfo.listEventType === 'added' && streamInfo.isRemote) {
@@ -43,10 +41,30 @@ function VideoCall({ hangUpCall, declineCall }) {
     stream.removeFromDiv('opVideo', 'remote-media-' + stream.streamId)
   }
   useEffect(() => {
+    if (chat.isRecieved) {
+      setLoaded(true)
+    }
+    const id = chat.isRecieved ? chat.data.from : chat.data.to
+    if (id) {
+      const options = {
+        route: 'getUserInfo',
+        params: { userId: id },
+        headers: { Authorization: `Bearer ${localStorage.getItem('SyncUp_Auth_Token')}` },
+        crypto: true
+      }
+      Axios(options).then(res => {
+        if (res.data.success) {
+          setUserData(res.data.body)
+        } else {
+          toast.error(res.data.message)
+        }
+      })
+    }
     socket.on('callAccepted', () => {
-      alert('accepted')
-      setConnected(true)
-      initializeVideo()
+      setChat({ ...chat, isAccepted: true })
+      initializeVideo().then(() => {
+        setLoaded(true)
+      })
     })
     socket.on('callEnded', (data) => {
       // const videos = document.querySelectorAll('video')
@@ -67,7 +85,7 @@ function VideoCall({ hangUpCall, declineCall }) {
       // })
     })
   }, [socket])
-  const initializeVideo = () => {
+  const initializeVideo = async () => {
 
     let localStream = null;
     const apikey = "58fe00be7be7c9805c1c0b98b195669a"
@@ -81,14 +99,11 @@ function VideoCall({ hangUpCall, declineCall }) {
 
       const conversation = session.getOrCreateConversation(conversationName, { meshOnlyEnabled: true })
 
-      setConversationName(conversation.getName())
-
       conversationRef.current = conversation
 
       conversation.on("streamListChanged", onStreamListChangedHandler)
       conversation.on("streamAdded", onStreamAddedHandler)
       conversation.on("streamRemoved", onStreamRemovedHandler)
-
       //Instantiate a local video stream object
       ua.createStream({
         constraints: {
@@ -97,13 +112,14 @@ function VideoCall({ hangUpCall, declineCall }) {
         }
       })
         .then((stream) => {
-          localStream = stream;
+          let strm = stream
           stream.attachToElement(document.getElementById('local-video-stream'));
           conversation.join()
             .then((response) => {
               conversation
-                .publish(localStream)
-                .then((stream) => {
+                .publish(strm)
+                .then(() => {
+                  setLocalStream(stream)
                   console.log('local published')
                 })
                 .catch((err) => {
@@ -119,24 +135,26 @@ function VideoCall({ hangUpCall, declineCall }) {
   }
   const acceptCall = function () {
     socket.emit('userAcceptedACall', chat.data)
-    setConnected(true)
-    setLoaded(true)
+    setChat({ ...chat, isAccepted: true })
     initializeVideo()
-
   }
   return (
     <>
+      {userData && <div className='userDetailsVidCall'>
+        <img src={userData.avatar_url} className='vidCallAvatar' alt="" />
+        <h4>{userData.username}</h4>
+        </div>}
       {callState.value && <div className="videoContainer">
         {isLoaded ? <>
           <div className="callOptions">
-            {chat.isRecieved && !callState.value.isAccepted ?
+            {chat.isRecieved && !chat.isAccepted ?
               <>
                 <button className="Accept" onClick={acceptCall} >Accept</button>
-                <button className="Decline" onClick={declineCall} >Decline</button>
+                <button className="Decline" onClick={props.declineCall} >Decline</button>
               </>
               :
               <>
-                <button onClick={hangUpCall}> <img src={imgDecline} alt="" /> </button>
+                <button onClick={props.hangUpCall}> <img src={imgDecline} alt="" /> </button>
               </>}
           </div>
           <div className="callUi">
@@ -150,7 +168,7 @@ function VideoCall({ hangUpCall, declineCall }) {
           </div></> : <div className="vidLoading">
           <span className="spinner" />
           <p className='text-light' >Connecting...</p>
-          <button className='btnHangup' onClick={hangUpCall}> <img src={imgDecline} alt="" /></button>
+          <button className='btnHangup' onClick={props.hangUpCall}> <img src={imgDecline} alt="" /></button>
 
         </div>}
       </div>
