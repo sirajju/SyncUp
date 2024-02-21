25cf14cbd0160fbb551903f2bec91da859b659c7
sirajju <munnas2aa@gmail.com>
2024-02-21 11:16:22 +0530

    Call logs fixed

--- client/src/Components/CallLogs/CallLog.jsx
+++ client/src/Components/CallLogs/CallLog.jsx
@@ -5,9 +5,9 @@ import { useSelector } from "react-redux"
 import videoCallIcon from '../../assets/Images/videocall.png'
 import './CallLog.css'
 
-export default function ({setGo,setChat}) {
+export default function ({ setGo, setChat }) {
     const [logs, setLogs] = useState([])
-    const userData = useSelector(state=>state.user)
+    const userData = useSelector(state => state.user)
     useEffect(() => {
         const options = {
             route: "getCallLogs",
@@ -21,22 +21,23 @@ export default function ({setGo,setChat}) {
                 toast.error(res.data.message)
             }
         })
-        document.addEventListener('keyup',()=>{
+        document.addEventListener('keyup', () => {
             setGo('')
         })
     }, [])
     return (
-        <>
+        <div data-aos="fade-down" data-aos-duration="700" className="callLogParent">
             {logs.map(el => (
-                <div data-aos="fade-down" data-aos-duration="700" className={`notificationItem ${el.data.isAccepted ? 'acceptedCall' :(el.data.from==userData.value._id ? "outgoingCall" :"incomingCall")} `}>
+                <div className={`callLogItem ${el.data.isAccepted ? 'acceptedCall' : (el.data.from == userData.value._id ? "outgoingCall" : "incomingCall")} `}>
                     <img src={el.opponentData.avatar_url} className='chatIcon' />
-                    <span className='text-center p-3' style={{ width: '100%' }}>{el.data.from==userData.value._id ?`Outgoing videocall to ${el.opponentData.username}` : (el.data.isAccepted ? `Incoming videocall from ${el.opponentData.username}` : `Missed videocall from ${el.opponentData.username}`)}</span>
+                    <span className='text-center p-3' style={{ width: '100%' }}>{el.data.from == userData.value._id ? `Outgoing videocall to ${el.opponentData.username}` : (el.data.isAccepted ? `Incoming videocall from ${el.opponentData.username}` : `Missed videocall from ${el.opponentData.username}`)}</span>
                     <div className="followRqstDiv" style={{ display: 'flex', flexDirection: 'column' }}>
-                        <span style={{ fontSize: '10px', fontWeight: 400, margin: '60px 50px 0 0 ', position: 'absolute' }}>{new Date(el.data.createdAt).toLocaleTimeString('en-US',{hour:"2-digit",minute:"2-digit",hour12:true})}</span>
+                        <span style={{ fontSize: '10px', fontWeight: 400, margin: '60px 10px 10px 0 ', whiteSpace: "nowrap", textOverflow: "ellipsis" }}>{new Date(el.data.createdAt).toLocaleTimeString('en-US', { hour: "2-digit", minute: "2-digit", hour12: true })}</span>
                     </div>
                     <img className="logVidCallIcon" src={videoCallIcon} alt="" />
                 </div>
             ))}
-        </>
+
+        </div>
     )
 }
