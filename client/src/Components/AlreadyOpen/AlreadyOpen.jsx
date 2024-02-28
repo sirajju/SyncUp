e2ec76d478763ee2aa9ea56f31b803801b170c9f
sirajju <munnas2aa@gmail.com>
2024-02-28 13:27:20 +0530

    Bug fixed

--- client/src/Components/AlreadyOpen/AlreadyOpen.jsx
+++ client/src/Components/AlreadyOpen/AlreadyOpen.jsx
@@ -14,6 +14,7 @@ function AlreadyOpen() {
     useEffect(()=>{
         dispatch(hideLoading())   
     },[])
+   
     return (
         <div className="otpContainer">
             <Box style={boxStyle}>
