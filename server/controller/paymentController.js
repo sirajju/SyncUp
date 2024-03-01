9d199864a7f4904ad800049dc1946cac94f44148
sirajju <munnas2aa@gmail.com>
2024-03-02 00:09:40 +0530

    Fixed signature verification failed err

--- server/controller/paymentController.js
+++ server/controller/paymentController.js
@@ -10,15 +10,18 @@ const createPaymentSession = async (req, res) => {
             const token = generateRandom(50, false)
             const em = `http://localhost:3000/plans?token=${token}&&u=${btoa(req.userEmail)}`
             console.log(em);
+
             await stripe.customers.create({
-                name: 'Customer Name',
+                name: 'Hello world',
+                description:"Premium payment",
                 address: {
-                  line1: 'Address Line 1',
-                  city: 'City',
-                  postal_code: 'Postal Code',
-                  country: 'IN', // India
-                }
-              })
+                    line1: 'Kozhikode',
+                    postal_code: '673580',
+                    city: 'Calicut',
+                    state: 'KERALA',
+                    country: 'INDIA',
+                },
+            })
             const session = await stripe.checkout.sessions.create({
                 payment_method_types: ["card"],
                 line_items: [
@@ -31,13 +34,12 @@ const createPaymentSession = async (req, res) => {
                 success_url: em,
                 cancel_url: `http://localhost:3000/plans?isCancelled=true`,
                 customer_email: req.userEmail,
-                currency:"inr",
-                shipping_address_collection: {
-                    allowed_countries: ['IN'] // Restrict to India
-                }
+                currency: "inr",
+
             })
             if (session) {
                 const userData = await User.findOne({ email: req.userEmail })
+                const existsData = await Premium.findOneAndDelete({userId:userData._id})
                 const premiumData = await new Premium({
                     userId: userData._id,
                     type: plan,
@@ -54,53 +56,53 @@ const createPaymentSession = async (req, res) => {
                 }
                 res.json({ message: "Error while creating session" })
             }
-        }else{
-            res.json({message:"Something went wrong"})
+        } else {
+            res.json({ message: "Something went wrong" })
         }
     } catch (error) {
         console.log(error);
         res.json({ message: error.message })
     }
 }
-const verifyPremium = async(req,res)=>{
+const verifyPremium = async (req, res) => {
     try {
-        const {token}=req.query
-        if(token){
-            const userData=await User.findOne({email:req.userEmail})
-            const premiumData = await Premium.findOne({userId:userData._id})
-            console.log(premiumData,token);
-            if(premiumData.authToken==token){
-                const updateData = await User.findOneAndUpdate({email:req.userEmail},{$set:{isPremium:true}})
-                const premiumUpdate = await Premium.findOneAndUpdate({userId:userData._id},{$set:{paymentStatus:"success",authToken:''}})
-                if(premiumUpdate&&updateData){
-                    res.json({success:true})
-                }else{
-                    res.json({message:"Error while updating state"})
+        const { token } = req.query
+        if (token) {
+            const userData = await User.findOne({ email: req.userEmail })
+            const premiumData = await Premium.findOne({ userId: userData._id })
+            console.log(premiumData, token);
+            if (premiumData.authToken == token) {
+                const updateData = await User.findOneAndUpdate({ email: req.userEmail }, { $set: { isPremium: true } })
+                const premiumUpdate = await Premium.findOneAndUpdate({ userId: userData._id }, { $set: { paymentStatus: "success", authToken: '' } })
+                if (premiumUpdate && updateData) {
+                    res.json({ success: true })
+                } else {
+                    res.json({ message: "Error while updating state" })
                 }
-            }else{
-                res.json({message:'Signature verification failed'})
+            } else {
+                res.json({ message: 'Signature verification failed' })
             }
-        }else{
-            res.json({message:'Something went wrong'})
+        } else {
+            res.json({ message: 'Something went wrong' })
         }
     } catch (error) {
         console.log(error);
-        res.json({message:error.message})
+        res.json({ message: error.message })
     }
 }
 
-const paymentCancelled = async(req,res)=>{
+const paymentCancelled = async (req, res) => {
     try {
-        const userData = await User.findOne({email:req.userEmail})
-        if(userData){
-            const premiumData = await Premium.findOneAndDelete({userId:userData._id})
-            if(premiumData){
-                res.json({message:`Payment is not completed yet`,success:true})
+        const userData = await User.findOne({ email: req.userEmail })
+        if (userData) {
+            const premiumData = await Premium.findOneAndDelete({ userId: userData._id })
+            if (premiumData) {
+                res.json({ message: `Payment is not completed yet`, success: true })
             }
         }
     } catch (error) {
         console.log(error);
-        res.json({message:error.message})
+        res.json({ message: error.message })
     }
 }
 
