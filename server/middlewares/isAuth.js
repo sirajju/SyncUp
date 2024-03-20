5062a02c133099b84d834155510669acadc55a2f
sirajju <munnas2aa@gmail.com>
2024-03-20 19:09:14 +0530

    Bug fixed

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -9,30 +9,17 @@ module.exports = async function (req, res, next) {
     try {
         const token = req.headers['authorization'].split('Bearer')[1].trim()
         const { username, email } = jwt.verify(token, process.env.JWT_SECRET)
-        const userData = await User.findOne({ $or: [{ email }, { username }] })
+        const userData = await User.findOne({ $or: [{ email }, { username }] }, { googleContacts: 0 })
         if (userData) {
             if (userData.isBlocked) {
                 await User.findOneAndUpdate({ email }, { $set: { logged_devices: 0 } })
                 res.status(203).json({ message: "User suspended", success: false })
-                const connData = await Connection.findOne({userId:userData._id})
+                const connData = await Connection.findOne({ userId: userData._id })
             } else {
-                const ls = await User.findOneAndUpdate({ email: userData.email }, { $set: { last_seen: Date.now() } })
-                if (req.path == '/isAlive') {
-                    if (!userData.isEmailVerified) {
-                        res.status(200).json({ success: false, err: "EMAILNOTVERERR", message: "Email is not verified yet!!" })
-                    }
-                    else if (req.query.getData) {
-                        const encData = crypto.AES.encrypt(JSON.stringify(userData), process.env.CRYPTO_SECRET)
-                        res.status(200).json({ body: encData.toString(), success: true })
-                    }
-                    else {
-                        res.status(200).json({ success: true })
-                    }
-                } else {
-                    req.userEmail = email
-                    req.userName = username
-                    next()
-                }
+               await User.findOneAndUpdate({ email: userData.email }, { $set: { last_seen: Date.now() } })
+                req.userEmail = email
+                req.userName = username
+                next()
             }
         } else {
             res.status(203).json({ message: "Session expired", success: false })
